828affaeff1b0fe6b907470968874c8fd620cb9c
Lauryn <lauryn2001arora@gmail.com>
2024-02-19 23:58:05 +0530

    first commit

---
+++ src/_root/pages/index.ts
@@ -0,0 +1,37 @@
+// export {default as Home} from './Home';
+// export {default as Explore} from './Explore';
+// export {default as AllUsers} from './AllUsers';
+// export {default as EditPost} from './EditPost';
+// export {default as Saved} from './Saved';
+// // export {default as CreatePost} from './CreatePost';
+// export {default as Profile} from './Profile';
+// export {default as UpdateProfile} from './UpdateProfile';
+// export {default as LikedPosts} from './LikedPosts';
+// export {default as PostDetails} from './PostDetails';
+
+// Import the components
+import Home from './Home';
+import Explore from './Explore';
+import AllUsers from './AllUsers';
+import EditPost from './EditPost';
+import Saved from './Saved';
+import CreatePost from './CreatePost'; // Commented out for now
+import Profile from './Profile';
+import UpdateProfile from './UpdateProfile';
+import LikedPosts from './LikedPosts';
+import PostDetails from './PostDetails';
+
+// Export the components
+export {
+  Home,
+  Explore,
+  AllUsers,
+  EditPost,
+  Saved,
+  CreatePost, // Commented out for now
+  Profile,
+  UpdateProfile,
+  LikedPosts,
+  PostDetails,
+};
+
